b50e5e6a6c60700ff0a081222083c4d7c5ddea9b
akashdeep singh <youngbrothersdev@gmail.com>
2025-05-15 18:02:17 +0530

    Update 3d_modeling_car.php

--- blogs/3d_Modeling_Car/3d_modeling_car.php
+++ blogs/3d_Modeling_Car/3d_modeling_car.php
@@ -14,7 +14,7 @@
       gtag('config', 'G-D0QKYRT3F8');
     </script>
      <title>Car modeling in blender</title>
-     <link rel="icon" href="icon.png" type="../../image/x-icon">
+     <link rel="icon" href="../../icon.png" type="image/x-icon">
     <!-- Required meta tags -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
