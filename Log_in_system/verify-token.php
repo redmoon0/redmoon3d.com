150eabbf05558ee63b33d962b9e186f86fa3ba5c
akashdeep singh <youngbrothersdev@gmail.com>
2025-05-20 15:09:00 +0530

    getting user data

--- Log_in_system/verify-token.php
+++ Log_in_system/verify-token.php
@@ -1,5 +1,6 @@
 <?php
 // Get the token sent from JS
+include "db.php"
 $input = json_decode(file_get_contents("php://input"), true);
 $id_token = $input['token'];
 
@@ -8,6 +9,18 @@ $response = file_get_contents("https://oauth2.googleapis.com/tokeninfo?id_token=
 $user = json_decode($response, true);
 
 if (isset($user['email'])) {
+    $email = $user['email'];
+    $name = $user['name'];
+    
+    if ($conn->connect_error) {
+        die(json_encode(['success' => false, 'error' => 'DB connection failed']));
+    }
+
+    // 4. Insert or ignore if already exists
+    $stmt = $conn->prepare("INSERT IGNORE INTO users (name, email) VALUES (?, ?)");
+    $stmt->bind_param("ss", $name, $email);
+    $stmt->execute();
+
     echo json_encode([
         'success' => true,
         'email' => $user['email'],
